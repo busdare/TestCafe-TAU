bfd56e636d31cb418e49fefa5eab754486aa52eb
Oludare Busari <Oludare@sharklasers.com>
2020-12-22 22:41:06 -0700

    Updated testcafe to the latest release and added node modules to gitignore

--- tests/TestSelector.js
+++ tests/TestSelector.js
@@ -8,7 +8,7 @@ fixture("First Fixture with TestCafe")
     .page("https://devexpress.github.io/testcafe/example/");
 test("First Test with TestCafe", async t => {
     await t
-        .typeText(developerName,'TAU')
+        .typeText(developerName, 'TAU')
         .click(osOption)
         .click(submitButton);
-    });
+});
